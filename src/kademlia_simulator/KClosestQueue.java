240b33be22becc86ca14831a58f71541783f1980
William Guglielmo <william@deselmo.com>
2019-04-30 05:09:27 +0200

    Refactor KClosestQueue.java

--- src/kademlia_simulator/KClosestQueue.java
+++ src/kademlia_simulator/KClosestQueue.java
@@ -1,23 +1,54 @@
 package kademlia_simulator;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
 /**
- * KClosestQueue
+ * KClosestQueue class representing an sorted list of k node wrt a specified target
  */
 public final class KClosestQueue implements Iterable<Node> {
-    private final int k;
-    private final Identifier target;
-    private TreeSet<DistanceNode> nodes;
-    private Map<Node, Set<Node>> traversedNodes;
 
-    public KClosestQueue(Node bootstrapNode, Identifier target, int k, Node originNode) {
-        if(bootstrapNode == null || target == null) 
+    /**
+     * size of this queue
+     */
+    private final int k;
+
+    /**
+     * specified target identifier
+     */
+    private final Identifier target;
+
+    /**
+     * TreeSet representing a sorted queue of different nodes
+     */
+    private final TreeSet<DistanceNode> nodes;
+
+    /**
+     * map that for each node contains through which nodes that node as been reached
+     */
+    private final Map<Node, Set<Node>> traversedNodes;
+
+
+    /**
+     * Consructs a new KClosestQueue with the specified size, the specified target,
+     * and adding in its queue the specified bootstrapNode reachable via the specified
+     * originNode.
+     * 
+     * @param bootstrapNode  first node to add to this queue
+     * @param target  specified target, to calculate the distances
+     * @param k  specified size of this queue
+     * @param originNode  node through which the boostrapNode is reachable
+     */
+    public KClosestQueue(final Node bootstrapNode, final Identifier target,
+                         final int k, final Node originNode) {
+
+        if(bootstrapNode == null || target == null || originNode == null) 
            throw new NullPointerException();
 
         if(!(k > 0))
@@ -28,50 +59,107 @@ public final class KClosestQueue implements Iterable<Node> {
         this.target = target;
         this.k = k;
 
+        // create a DistanceNode for the bootstrapNode
         this.nodes.add(new DistanceNode(bootstrapNode, this.target));
-        Set<Node> traversedNodes = new HashSet<>();
-        traversedNodes.add(originNode);
-        this.traversedNodes.put(bootstrapNode, traversedNodes);
+
+        // create the set of node through the bootstrap node is reachable
+        // now it contains only the originNode
+        final Set<Node> currentTraversedNodes = new HashSet<>();
+        currentTraversedNodes.add(originNode);
+
+        // map the bootstrapNode to its traversedNodes
+        this.traversedNodes.put(bootstrapNode, currentTraversedNodes);
     }
 
 
-    public boolean add(Node node, Node queriedNode) {
+    /**
+     * Tries to add the specified node to this queue.
+     * 
+     * If the specified node if closer than at least one node in this queue,
+     * the specified node is added in this queue.
+     * 
+     * Anyway how the specified node is reachable is saved in this object.
+     * 
+     * @param node  specified node that we are trying to add
+     * @param queriedNode  node through which the specified node is reachable
+     * @return  {@code false} if the specified node is already in this queue
+     */
+    public final boolean add(final Node node, final Node queriedNode) {
         if (nodes == null)
             throw new NullPointerException();
 
-            
+        // add the node to the internal TreeSet,
+        // if the specified node is already present return {@code false}
         if (!this.nodes.add(new DistanceNode(node, target)))
             return false;
-            
-        Set<Node> traversedNodes = new HashSet<>(this.getTraversedNodes(queriedNode));
-        traversedNodes.add(queriedNode);
-        this.traversedNodes.put(node, traversedNodes);
 
+
+        // compute the traversed, map in this.traversedNode that the specified node
+        // is reachable through the queriedNode plus the traversed nodes for queriedNode
+        Set<Node> currentTraversedNodes =
+                new HashSet<>(this.getTraversedNodes(queriedNode));
+        currentTraversedNodes.add(queriedNode);
+        this.traversedNodes.put(node, currentTraversedNodes);
+
+        // if the size of this queue is bigger that k, reduce it to k
         this.updateNodes();
 
         return true;
     }
 
 
-    public Node getClosestNode() {
+    /**
+     * Returns a list containing the nodes in this queue.
+     * The returned list has size smaller than or equal to k.
+     * 
+     * @return  the nodes contained in this queue
+     */
+    public final List<Node> getNodes() {
+        final List<Node> closestNodes = new ArrayList<>();
+        for(final DistanceNode distanceNode : this.nodes)
+            closestNodes.add(distanceNode.getNode());
+
+        return closestNodes;
+    }
+
+
+    /**
+     * Returns the closest node to the target in this queue.
+     * 
+     * @return  the closest node to the target in this queue
+     */
+    public final Node getClosestNode() {
         return this.nodes.first().getNode();
     }
 
 
-    public Set<Node> getTraversedNodes(Node queriedNode) {
-        if(queriedNode == null) throw new RuntimeException();
+    /**
+     * Returns the set of nodes needed to be traversed to reach the specified node.
+     * 
+     * @param node  the specified node
+     * @return  the nodes needed to be traversed to reach the specified node
+     */
+    public final Set<Node> getTraversedNodes(final Node node) {
+        if(node == null) throw new RuntimeException();
 
-        return this.traversedNodes.get(queriedNode);
+        return this.traversedNodes.get(node);
     }
 
 
-    private void updateNodes() {
+    /**
+     * Removes the last nodes of the internal TreeSet until the size of the
+     * internal TreeSet is reduced to k.
+     */
+    private final void updateNodes() {
         while(this.nodes.size() > this.k) {
             this.nodes.pollLast().getNode();
         }
     }
 
 
+    /**
+     * Implementation of the iterator method of the Iterable interface.
+     */
     @Override
     public Iterator<Node> iterator() {
         return new InternalIterator();
@@ -79,8 +167,12 @@ public final class KClosestQueue implements Iterable<Node> {
 
 
 
-    private class InternalIterator implements Iterator<Node> {
-        private Iterator<DistanceNode> iterator;
+    /**
+     * private class used to covert the iterator of DistanceNode of the internal TreeSet
+     * to an iterator of Node.
+     */
+    private final class InternalIterator implements Iterator<Node> {
+        private final Iterator<DistanceNode> iterator;
 
         private InternalIterator() {
             this.iterator = nodes.iterator();
@@ -88,12 +180,12 @@ public final class KClosestQueue implements Iterable<Node> {
 
 
         @Override
-        public boolean hasNext() {
+        public final boolean hasNext() {
             return this.iterator.hasNext();
         }
 
         @Override
-        public Node next() {
+        public final Node next() {
             return this.iterator.next().getNode();
         }
     }
